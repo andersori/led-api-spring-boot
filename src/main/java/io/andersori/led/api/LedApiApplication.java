0c5e544dc23f9d9d80448a573c5057bbc948596e
Anderson Soriano <19699525+andersori@users.noreply.github.com>
2020-03-02 08:31:40 -0300

    disconnecting redis when the application is finished

--- src/main/java/io/andersori/led/api/LedApiApplication.java
+++ src/main/java/io/andersori/led/api/LedApiApplication.java
@@ -2,12 +2,19 @@ package io.andersori.led.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import redis.clients.jedis.Jedis;
 
 @SpringBootApplication
 public class LedApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LedApiApplication.class, args);
+		ConfigurableApplicationContext contex = SpringApplication.run(LedApiApplication.class, args);
+
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			contex.getBean(Jedis.class).disconnect();
+		}));
 	}
 
 }
